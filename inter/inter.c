47e04c3329f875a0cfde46d21811f55efdcf9382
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2021-12-10 17:48:22 +1030

    Add files via upload

--- inter/inter.c
+++ inter/inter.c
@@ -1,12 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 17:41:19 by prossi            #+#    #+#             */
+/*   Updated: 2021/12/10 17:46:50 by prossi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int iter(char *str, char c, int len)
+int	main(int ac, char **av)
 {
-    int i;
-    
-    i = 0;
-    while (str[i] && (i < len || len == -1))
-        if (str[i++] == c) 
-            return (1);
-    return (0); 
+	if (ac == 3)
+	{
+		int	i, j, tab[255];	
+		i = 0;
+		while (i < 255)
+			tab[i++] = 0;
+		i = 2;
+		while (i > 0)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				if (i == 2 && tab[(unsigned char)av[i][j]] == 0)
+					tab[(unsigned char)av[i][j]] = 1;
+				if (i == 1 && tab[(unsigned char)av[i][j]] == 1)
+				{
+					tab[(unsigned char)av[i][j]] = 2;
+					write(1, &av[i][j], 1);
+				}
+				j++;
+			}
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
