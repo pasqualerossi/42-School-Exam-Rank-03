b223a6478e16c18849351f2ad64d81ce1b5440f5
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-04 14:32:19 +1030

    Add files via upload

--- get_next_line/getnextline.c
+++ get_next_line/getnextline.c
@@ -5,92 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/09 20:19:50 by prossi            #+#    #+#             */
-/*   Updated: 2021/12/09 20:50:34 by prossi           ###   ########.fr       */
+/*   Created: 2022/01/04 14:06:02 by prossi            #+#    #+#             */
+/*   Updated: 2022/01/04 14:30:20 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <fcntl.h>
-
-int	find_newline(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (-1);
-	while (str[i] != '\0')
-	{
-		if (str[i] == '\n')
-			return (i);
-		i++;
-	}
-	return (-1);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_calc_line(char *line, char buf)
-{
-	char	*res;
-	int		i;
-
-	i = 0;
-	res = malloc(ft_strlen(line) + 1 + 1);
-	while (line[i] != '\0')
-	{
-		res[i] = line[i];
-		i++;
-	}
-	res[i] = buf;
-	i++;
-	res[i] = '\0';
-	free(line);
-	return (res);
-}
 
 char	*get_next_line(int fd)
 {
-	char	buf;
-	int		bytes_read;
+	char	save[999999];
+	char	buff[1];
 	char	*line;
+	int		i;
 
+	i = 0;
 	if (fd < 0)
-		return (0);
-	bytes_read = 1;
-	line = malloc(1);
-	line[0] = '\0';
-	while (find_newline(line) == -1 && bytes_read != 0)
-	{	
-		bytes_read = read(fd, &buf, 1);
-		if (bytes_read <= 0)
-		{
-			if (line[0] == '\0')
-			{
-				free(line);
-				return (NULL);
-			}
-			return (line);
-		}
-		else if (bytes_read == -1)
-		{
-			free(line);
-			return (NULL);
-		}
-		line = ft_calc_line(line, buf);
+		return (NULL);
+	save[i] = '\0';
+	while (read(fd, buff, 1) == 1)
+	{
+		save[i] = buff[0];
+		save[i + 1] = '\0';
+		if (save[i] == '\n')
+			break ;
+		i++;
 	}
+	if (save[0] == '\0')
+		return (NULL);
+	line = malloc(i + 1);
+	i = 0;
+	while (save[i])
+	{
+		line[i] = save[i];
+		i++;
+	}
+	line[i] = '\0';
 	return (line);
 }
