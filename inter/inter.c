f5cb6f24392fcbc6ce146326ab45719f54f36ee4
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-05 13:49:05 +1030

    Update inter.c

--- inter/inter.c
+++ inter/inter.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
 int	main(int argc, char *argv[])
