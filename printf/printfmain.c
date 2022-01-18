f345677fde9fd02b0e4fa4eea5ce0af07fb1b4fb
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-19 08:51:29 +1030

    Delete printf directory

--- printf/printfmain.c
+++
@@ -1,9 +0,0 @@
-int	main(void)
-{
-	int	data = 1293;
-	int min_int = -2147483648;
-	printf("bytes printed %d\n", (ft_printf("%s\n", "Pasquale Rossi")));
-	ft_printf("bytes printed %d\n", (ft_printf("%s\n", "Emilio")));
-	ft_printf("here is a hex number %x\n", data);
-	printf("bytes printed %d\n", (ft_printf("lowest int is %d\n", min_int)));				
-}
