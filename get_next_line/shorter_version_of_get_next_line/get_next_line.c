79b9a27bd9a80fd0646a5dae5419dde6e133b462
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-04-27 17:27:54 +0930

    Add files via upload

---
+++ get_next_line/shorter_version_of_get_next_line/get_next_line.c
@@ -0,0 +1,8 @@
+#include "get_next_line.h"
+
+char *get_next_line(int fd) 
+{
+    char *s = malloc(10000), *c = s;
+    while (read(fd, c, 1) > 0 && *c++ != '\n');
+  	return c > s ? (*c = 0, s) : (free(s), NULL);
+}
---
+++ get_next_line/shorter_version_of_get_next_line/get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
