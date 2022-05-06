abe4811f184678fd9fd79d9185775f8e75b7e446
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-05-06 13:41:00 +0930

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,27 +1,24 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	int	rd;
-	int	i = 0;
-	char	c;
-	char	*buffer = malloc(9999);
+    int 	i = 0;
+    int 	rd = 0;
+    char	character;
+    char 	*buffer = malloc(10000);
 
-	while ((rd = read(fd, &c, 1) > 0))
-	{
-		buffer[i] = c;
-		i++;
-		if (c == '\n')
-			break ;
-	}
-	if ((!buffer[i - 1] && !rd) || rd == -1)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	i++;
-	buffer[i] = '\0';
-	return (buffer);
+    while ((rd = read(fd, &character, 1)) > 0)
+    {
+        buffer[i++] = character;
+        if (character == '\n')
+            break;
+    }
+    if ((!buffer[i - 1] && !rd) || rd == -1)
+    {
+        free(buffer);
+        return (NULL);
+    }
+    buffer[i] =  '\0';
+    return(buffer);
 }
