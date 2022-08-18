b9ec283f3823a9c3cda1fd210e109367fd6e36dd
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-08-18 16:24:11 +0930

    Update shorter_printf.c

--- ft_printf/shorter_printf.c
+++ ft_printf/shorter_printf.c
@@ -1,22 +1,22 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void    ft_putnbr(unsigned digits, unsigned length, char *sign, int *g_var)
+void    ft_put_number(unsigned digits, unsigned length, char *sign, int *g_var)
 {
     if (digits >= length)
     {
-	ft_putnbr(digits / length, length, sign, g_var);
+	ft_put_number(digits / length, length, sign, g_var);
     }
     *g_var += (int)write (1, &sign[digits % length], 1);
 }
 
-void    ft_dig(long digits, int length, char *sign, int *g_var)
+void    ft_digits(long digits, int length, char *sign, int *g_var)
 {
     (digits < 0) ? (*g_var += (int)write(1, "-", 1), \
-    ft_putnbr(-digits, length, sign,g_var)) : ft_putnbr(digits, length, sign, g_var);
+    ft_put_number(-digits, length, sign,g_var)) : ft_put_number(digits, length, sign, g_var);
 }
 
-size_t ft_putstr(char *string, int length)
+size_t ft_put_string(char *string, int length)
 {
     while (string && string[length] && ++length);
     return (string ? write(1, string, length) : write(1, "(null)", 6));
@@ -32,15 +32,15 @@ int ft_printf(const char *format, ...)
     {
         if (*format == '%' && *(format + 1) == 's' && (format += 2))
 	{
-            g_var += (int)ft_putstr(va_arg(ap, char *), 0);
+            g_var += (int)ft_put_string(va_arg(ap, char *), 0);
 	}
 	else if (*format == '%' && *(format + 1) == 'x' && (format += 2))
         {
-	    ft_putnbr(va_arg(ap, int), 16, "0123456789abcdef", &g_var);
+	    ft_put_number(va_arg(ap, int), 16, "0123456789abcdef", &g_var);
 	}
 	else if (*format == '%' && *(format + 1) == 'd' && (format += 2))
         {    
-	    ft_dig(va_arg(ap, int), 10, "0123456789", &g_var);
+	    ft_digits(va_arg(ap, int), 10, "0123456789", &g_var);
 	}
 	else
             g_var += (int)write(1, format++, 1);
