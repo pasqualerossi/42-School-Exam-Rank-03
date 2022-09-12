04ba9b2f49e868750ab368985de3c17b740a24de
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-09-12 17:01:40 +0930

    Update get_next_line.c

--- get_next_line/shorter_version_of_get_next_line/get_next_line.c
+++ get_next_line/shorter_version_of_get_next_line/get_next_line.c
@@ -2,7 +2,7 @@
 
 char *get_next_line(int fd) 
 {
-    char *s = malloc(10000), *c = s;
-    while (read(fd, c, 1) > 0 && *c++ != '\n');
-    return c > s ? (*c = 0, s) : (free(s), NULL);
+    char *string = malloc(10000), *copy = string;
+	while (read(fd, copy, 1) > 0 && *copy++ != '\n');
+	return (copy > string) ? (*copy = 0, string) : (free(string), NULL);
 }
