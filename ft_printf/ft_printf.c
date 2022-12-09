66f39238344bc23ea2069d9d3ae7c7e4a085ac0e
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-12-09 16:14:32 +1030

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,34 +3,34 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-void	put_str(char *str, int *len)
+void	put_string(char *string, int *length)
 {
-	if (!str)
-		str = "(null)";
-	while (*str)
-		*len += write(1, str++, 1);
+	if (!string)
+		string = "(null)";
+	while (*string)
+		*length += write(1, string++, 1);
 }
 
-void	put_digit(long long int nbr, int base, int *len)
+void	put_digit(long long int number, int base, int *length)
 {
-	char	*hexa = "0123456789abcdef";
+	char	*hexadecimal = "0123456789abcdef";
 
-	if (nbr < 0)
+	if (number < 0)
 	{
-		nbr *= -1;
-		*len += write(1, "-", 1);
+		number *= -1;
+		*length += write(1, "-", 1);
 	}
-	if (nbr >= base)
-		put_digit((nbr / base), base, len);
-	*len += write(1, &hexa[nbr % base], 1);
+	if (number >= base)
+		put_digit((number / base), base, length);
+	*length += write(1, &hexadecimal[number % base], 1);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int		len = 0;
+	int length = 0;
 	
-	va_list		ptr;
-	va_start(ptr, format);
+	va_list	pointer;
+	va_start(pointer, format);
 	
 	while (*format)
 	{
@@ -38,15 +38,15 @@ int	ft_printf(const char *format, ...)
 		{
 			format++;
 			if (*format == 's')
-				put_str(va_arg(ptr, char *), &len);
+				put_string(va_arg(pointer, char *), &length);
 			else if (*format == 'd')
-				put_digit((long long int)va_arg(ptr, int), 10, &len);
+				put_digit((long long int)va_arg(pointer, int), 10, &length);
 			else if (*format == 'x')
-				put_digit((long long int)va_arg(ptr, unsigned int), 16, &len);
+				put_digit((long long int)va_arg(pointer, unsigned int), 16, &length);
 		}
 		else
-			len += write(1, format, 1);
+			length += write(1, format, 1);
 		format++;
 	}
-	return (va_end(ptr), len);
+	return (va_end(pointer), length);
 }
