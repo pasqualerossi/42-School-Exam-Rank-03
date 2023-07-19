cd630ecd1115e99b426098f53f01aaa9306f6d4c
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-07-19 18:31:14 +0930

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -2,21 +2,21 @@
 
 char *get_next_line(int fd)
 {
-    int     i = 0;
-    int     rd = 0;
-    char    character;
-    char    *buffer = malloc(100000);
-    
+    int i = 0;
+    int rd = 0;
+    char character;
+
     if (BUFFER_SIZE <= 0)
         return (NULL);
+    char *buffer = malloc(100000); // check before the current BUFFER_SIZE and then malloc
     while ((rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
     {
         buffer[i++] = character;
         if (character == '\n')
-            break ;
+            break;
     }
-    buffer[i] =  '\0';
+    buffer[i] = '\0';
     if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
         return (free(buffer), NULL);
-    return(buffer);
+    return (buffer);
 }
