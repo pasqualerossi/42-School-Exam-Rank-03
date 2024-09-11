89b5c1db05a29e6337274d0594cf09245470200e
Nzola Kiampava <nzolakiampava@gmail.com>
2024-09-11 16:17:51 +0100

    Update get_next_line.h
    get_next_line.h updated!

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,11 +1,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <unistd.h>
-# include <stdlib.h>
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-
+# include <unistd.h> 	// for read
+# include <stdlib.h>	// for malloc	
+# include <stdio.h>	// for printf and End of File(EOF)
+# include <fcntl.h>	// For open txt file
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 #endif
